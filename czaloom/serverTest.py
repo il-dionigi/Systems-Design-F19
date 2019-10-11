3fe2837b17eb6ed46cb641e9c80d593ecd48ff92
Charles Zaloom <czaloom4@gmail.com>
2019-10-11 15:10:49 -0700

    update server

--- czaloom/serverTest.py
+++ czaloom/serverTest.py
@@ -6,17 +6,18 @@ serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Assigns a port for the server that listens to clients connecting to this port.
 
-serv.bind((’0.0.0.0’, 8080)) 
+serv.bind(('0.0.0.0', 8080)) 
 serv.listen(5)
 while True:
 	conn, addr = serv.accept() 
-	from_client = ’’
+	from_client = ''
 	while True:
 		data = conn.recv(4096)
 		if not data: 
 			break 
-		from_client += data 
-		print(from_client) 
-		conn.send("I am SERVER\n")
+		from_client += data.decode('utf-8')
+		print(from_client)
+		output = 'I am SERVER' 
+		conn.send(output.encode('utf-8'))
 	conn.close()
-	print(’client disconnected’)
+	print("client disconnected")
