d11ce9a76e2cc557b208d3568e1735f22fe039a9
il-dionigi <brian.dionigi@gmail.com>
2019-10-11 16:09:53 -0700

    Update clientTest.py

--- braymond/clientTest.py
+++ braymond/clientTest.py
@@ -1,7 +1,8 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('192.168.43.248', 8081))
-client.send("I am CLIENT\n")
+output = 'I am CLIENT'
+client.sendall(output.encode('utf-8'))
 from_server = client.recv(4096)
 client.close()
-print(from_server)
+print(from_server.decode('utf-8'))
