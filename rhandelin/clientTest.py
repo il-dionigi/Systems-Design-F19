4ff1b454b49691f1c5b3e0f96e5e3a56953bbeac
rhandelin <richard@handelin.se>
2019-10-11 23:49:59 +0100

    newstuff

---
+++ rhandelin/clientTest.py
@@ -0,0 +1,8 @@
+import socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('192.168.43.55', 8080))
+output = 'I am CLIENT'
+client.sendall(output.encode('utf-8'))
+from_server = client.recv(4096)
+client.close()
+print(from_server.decode('utf-8'))
