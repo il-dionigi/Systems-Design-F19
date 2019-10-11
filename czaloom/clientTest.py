522ce86fcdd6e55b32a7ecdb5c6e790eccb80e62
Charles Zaloom <czaloom4@gmail.com>
2019-10-11 23:09:36 +0100

    update client

--- czaloom/clientTest.py
+++ czaloom/clientTest.py
@@ -1,7 +1,8 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('0.0.0.0', 8080))
-client.send("I am Client\n")
+client.connect(('172.20.10.2', 8080))
+output = 'I am CLIENT'
+client.sendall(output.encode('utf-8'))
 from_server = client.recv(4096)
 client.close()
-print(from_server)
+print(from_server.decode('utf-8'))
