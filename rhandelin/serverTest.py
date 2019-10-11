cca64cefc860a72c67a0914e2701c02a9f67cb92
rhandelin <richard@handelin.se>
2019-10-11 15:44:57 -0700

    Update serverTest.py

--- rhandelin/serverTest.py
+++ rhandelin/serverTest.py
@@ -1,7 +1,7 @@
 import socket
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serv.bind(('0.0.0.0', 8080))
+serv.bind(('192.168.43.55', 8080))
 serv.listen(5)
 while True:
 	conn, addr = serv.accept()
