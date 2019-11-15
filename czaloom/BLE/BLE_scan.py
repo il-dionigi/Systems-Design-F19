3a1e16429c356904c3fd1b2581077eee2d444ce9
Charles Zaloom <czaloom4@gmail.com>
2019-11-15 04:52:09 +0000

    ...

--- czaloom/BLE/BLE_scan.py
+++ czaloom/BLE/BLE_scan.py
@@ -4,15 +4,17 @@ class ScanDelegate(DefaultDelegate):
         def __init__(self):
                 DefaultDelegate.__init__(self) 
 
-        def handleDiscovery(self,dev,isNewDev,isNewData):
-                if isNewDev:
-                        print("Discovered Device" , dev.addr)
-                elif isNewData:
-                        print("Recieved New Data from" , dev.addr)
+        #def handleDiscovery(self,dev,isNewDev,isNewData):
+                #if isNewDev:
+                        #print("Discovered Device" , dev.addr)
+                #elif isNewData:
+                        #print("Recieved New Data from" , dev.addr)
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(10.0)
 
 for dev in devices:
-        print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-        for (adtype, desc, value) in dev.getScanData():
-       		print("  %s = %s" % (desc, value))
+        if dev.addr == "b8:27:eb:28:ee:43":
+		print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+        	for (adtype, desc, value) in dev.getScanData():
+       			print("  %s = %s" % (desc, value))
+			print(value)
