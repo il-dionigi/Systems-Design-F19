211204aeb6f5100e8c03cce288f2b0d0dbea6451
Charles Zaloom <czaloom4@gmail.com>
2019-11-21 14:20:42 -0800

    Created BLE.py

---
+++ czaloom/BLE/BLE.py
@@ -0,0 +1,73 @@
+import os
+import math
+import time 
+
+def hex_to_char(c):
+	if c < 10:
+		return str(c)
+	elif c == 10:
+		return "a"
+	elif c == 11:
+		return "b"
+	elif c == 12:
+		return "c"
+	elif c == 13:
+		return "d"	
+	elif c == 14:
+		return "e"
+	elif c == 15:
+		return "f"
+	else:
+		return "0"
+
+def int_to_byte(x):
+	byte = ""
+	if(x < 16):
+		byte = "0" + hex_to_char(x)
+	else:
+		byte = hex_to_char(int(x / 16)) + hex_to_char(x % 16)
+	return byte
+
+def BLE_broadcast_setup():
+	os.system("sudo hciconfig hci0 up")
+	os.system("sudo hciconfig hci0 leadv 3")
+	return
+
+def BLE_broadcast(data, UUID = "ABC4", Number_of_Broadcast_Cycles = 3, Time_Between_Transmissions = 10):
+	# Broadcasts data in 20 byte payloads over Number_of_Broadcast_Cycles with a Time_Between_Transmissions in seconds
+	# UUID is a device identifier, should be 2 bytes no spaces. May be changed if signal is relayed.
+	UUID = str(UUID[0:2]) + " " + str(UUID[2:4]) # Max 4 bytes (in hex)
+	msg = data
+
+	number_of_messages = int(math.ceil(len(msg) / 20.0))
+	if len(msg) % 20 != 0:
+		for i in range(20 - len(msg) % 20):
+			msg = msg + "_"
+
+	state = "Transmitting " + str(number_of_messages) + " messages"
+	print(state)
+
+	ServiceID_ = " " + int_to_byte(number_of_messages) + " "
+
+	#Add automatic message and id parsing here
+
+	preamble = "sudo hcitool -i hci0 cmd 0x08 0x0008 1f 02 01 06 03 03 "
+	preamble = preamble + UUID #Configureable (Service data type, MUUID, MUUID)
+	preamble = preamble + " 23 16 "
+
+	for iteration in range(Number_of_Broadcast_Cycles):
+		for i in range(number_of_messages):
+
+			ServiceID = int_to_byte(i+1) + ServiceID_
+			ServiceID = UUID
+			command = preamble + ServiceID
+			for l in msg[i*20:i*20+20]:
+				hexnum = str(hex(ord(l)))
+				command = command + hexnum[2] + hexnum[3] + " "
+
+			#print(command) # Uncomment if not on Pi
+			os.system(command) # Uncomment if on Pi
+			time.sleep(Time_Between_Transmissions)
+
+BLE_broadcast_setup()
+BLE_broadcast("Hello! This is our ECE180DA Project!")
