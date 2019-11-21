a5528d7ba5f44b8005840a7ae8835dcc56ecc6e0
Charles Zaloom <czaloom4@gmail.com>
2019-11-21 14:27:56 -0800

    Update BLE.py
    Final Configuration.
    Three Defined User Functions

--- czaloom/BLE/BLE.py
+++ czaloom/BLE/BLE.py
@@ -1,6 +1,10 @@
+from bluepy.btle import Scanner, DefaultDelegate
 import os
 import math
 import time 
+import numpy as np
+
+# --- START OF BROADCAST DEFINITIONS ---
 
 def hex_to_char(c):
 	if c < 10:
@@ -69,5 +73,98 @@ def BLE_broadcast(data, UUID = "ABC4", Number_of_Broadcast_Cycles = 3, Time_Betw
 			os.system(command) # Uncomment if on Pi
 			time.sleep(Time_Between_Transmissions)
 
+# --- END OF BROADCAST DEFINITIONS ---
+
+# --- START OF LISTEN DEFINITIONS ---
+
+def charhex_to_int(c):
+    if c == 'A' or c == 'a':
+        return 10
+    elif c == 'B' or c == 'b':
+        return 11
+    elif c == 'C' or c == 'c':
+        return 12
+    elif c == 'D' or c == 'd':
+        return 13
+    elif c == 'E' or c == 'e':
+        return 14
+    elif c == 'F' or c == 'f':
+        return 15
+    else:
+        return int(c)
+
+def get_letter(s):
+    ascii_ = 16*charhex_to_int(s[0]) + charhex_to_int(s[1])
+    return chr(ascii_)
+def get_number(s):
+    return 16*charhex_to_int(s[0]) + charhex_to_int(s[1])
+
+def get_ID(s):
+    return [get_number(s[0:2]),get_number(s[2:4])]
+def get_Message(s):
+    MSG = ""
+    for i in range(4,30,2):
+        MSG = MSG + get_letter(s[i:i+2])
+    return MSG
+
+def is_new_message(id_, id_list):
+    m_id = id_[0]
+    m_total = id_[1]
+    if m_total == 0:
+        return 0
+    for i in id_list:
+        if i == m_id:
+            return 0
+    return 1
+
+class ScanDelegate(DefaultDelegate):
+        def __init__(self):
+                DefaultDelegate.__init__(self) 
+
+def BLE_listen(UUID = "ABC4"):
+	UUID = UUID[2:4] + UUID[0:2]
+	scanner = Scanner().withDelegate(ScanDelegate())
+
+	id_list = []
+	msg_arr = []
+	start_flag = 1
+	max_num_messages = 0
+	message_count = 0
+
+	while(1):
+	    devices = scanner.scan(10.0)
+
+	    for dev in devices:
+	        id_ = [0,0]
+	        found_flag = 0
+	        for (adtype, desc, packet) in dev.getScanData():
+	            if desc == "Complete 16b Services":
+	                if packet[4:8] == master_pi_UUID:
+	                    found_flag = 1
+	            if desc == "16b Service Data" and found_flag:
+	                id_ = get_ID(packet)
+	        if is_new_message(id_, id_list) and found_flag:
+	            if start_flag:
+	                max_num_messages = id_[1]
+	                start_flag = 0
+	                id_list = [None] * max_num_messages
+	                msg_arr = [None] * max_num_messages
+	            id_list[id_[0]] = m_id
+	            msg_arr[id_[0]] = get_Message(packet)
+	            message_count = message_count + 1
+	    if message_count == max_num_messages:
+	        break
+
+	message = ""
+	for i in msg_arr:
+	    message = message + i
+
+	return message
+
+# --- END OF LISTEN DEFINITIONS ---
+
+
 BLE_broadcast_setup()
 BLE_broadcast("Hello! This is our ECE180DA Project!")
+
+#print(BLE_listen())
