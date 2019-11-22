5ba6c6e80367948c8518a87d2985ddb5978d83e2
Charles Zaloom <czaloom4@gmail.com>
2019-11-22 15:14:38 -0800

    udate

--- czaloom/BLE/BLE.py
+++ czaloom/BLE/BLE.py
@@ -77,48 +77,48 @@ def broadcast(data, UUID = "ABC4", Number_of_Broadcast_Cycles = 3, Time_Between_
 # --- START OF LISTEN DEFINITIONS ---
 
 def charhex_to_int(c):
-    if c == 'A' or c == 'a':
-        return 10
-    elif c == 'B' or c == 'b':
-        return 11
-    elif c == 'C' or c == 'c':
-        return 12
-    elif c == 'D' or c == 'd':
-        return 13
-    elif c == 'E' or c == 'e':
-        return 14
-    elif c == 'F' or c == 'f':
-        return 15
-    else:
-        return int(c)
+	if c == 'A' or c == 'a':
+		return 10
+	elif c == 'B' or c == 'b':
+		return 11
+	elif c == 'C' or c == 'c':
+		return 12
+	elif c == 'D' or c == 'd':
+		return 13
+	elif c == 'E' or c == 'e':
+		return 14
+	elif c == 'F' or c == 'f':
+		return 15
+	else:
+		return int(c)
 
 def get_letter(s):
-    ascii_ = 16*charhex_to_int(s[0]) + charhex_to_int(s[1])
-    return chr(ascii_)
+	ascii_ = 16*charhex_to_int(s[0]) + charhex_to_int(s[1])
+	return chr(ascii_)
 def get_number(s):
-    return 16*charhex_to_int(s[0]) + charhex_to_int(s[1])
+	return 16*charhex_to_int(s[0]) + charhex_to_int(s[1])
 
 def get_ID(s):
-    return [get_number(s[0:2]),get_number(s[2:4])]
+	return [get_number(s[0:2]),get_number(s[2:4])]
 def get_Message(s):
-    MSG = ""
-    for i in range(4,30,2):
-        MSG = MSG + get_letter(s[i:i+2])
-    return MSG
+	MSG = ""
+	for i in range(4,30,2):
+		MSG = MSG + get_letter(s[i:i+2])
+	return MSG
 
 def is_new_message(id_, id_list):
-    m_id = id_[0]
-    m_total = id_[1]
-    if m_total == 0:
-        return 0
-    for i in id_list:
-        if i == m_id:
-            return 0
-    return 1
+	m_id = id_[0]
+	m_total = id_[1]
+	if m_total == 0:
+		return 0
+	for i in id_list:
+		if i == m_id:
+			return 0
+	return 1
 
 class ScanDelegate(DefaultDelegate):
-        def __init__(self):
-                DefaultDelegate.__init__(self) 
+		def __init__(self):
+				DefaultDelegate.__init__(self) 
 
 def listen(UUID = "ABC4"):
 	UUID = UUID[2:4] + UUID[0:2]
@@ -131,16 +131,16 @@ def listen(UUID = "ABC4"):
 	message_count = 0
 
 	while(1):
-	    devices = scanner.scan(10.0)
+		devices = scanner.scan(10.0)
 
-	    for dev in devices:
-	    	if dev.addr == "b8:27:eb:28:ee:43":
-            	print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-            	for (adtype, desc, value) in dev.getScanData():
-                	print("  %s = %s" % (desc, value))
-           	 	return value
+		for dev in devices:
+			if dev.addr == "b8:27:eb:28:ee:43":
+				print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
+				for (adtype, desc, value) in dev.getScanData():
+					print("  %s = %s" % (desc, value))
+				return value
 
-	       
+		   
 
 # --- END OF LISTEN DEFINITIONS ---
 
