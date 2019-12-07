c82c1316d2fd1cf2fa9174cfece8885ecaf6289e
Charles Zaloom <czaloom4@gmail.com>
2019-12-06 16:39:47 -0800

    qdwf

--- Mk1 Design/pixelPi/pixel.py
+++ Mk1 Design/pixelPi/pixel.py
@@ -46,50 +46,51 @@ def remote_callback(code):
 	# Sparkfun 9 button remote
 
 	if code == 16582903:
-		#Pressed: 1
+		print("Pressed: 1")
 		routine = 1
 
 	elif code == 16615543:
-		#Pressed: 2
+		print("Pressed: 2")
 		routine = 2
 		
-	elif code == 16599223:
-		#Pressed: 3
+	elif code == 16605343:
+		print("Pressed: 3")
 		routine = 3
 
 	elif code == 16591063:
-		#Pressed: 4
+		print("Pressed: 4")
 		routine = 4
 
 	elif code == 16623703:
-		#Pressed: 5
+		print("Pressed: 5")
 		routine = 5
 
 	elif code == 16607383:
-		#Pressed: 6
+		print("Pressed: 6")
 		routine = 6
 
 	elif code == 16586983:
-		#Pressed: 7
+		print("Pressed: 7")
 		routine = 7
 
 	elif code == 16619623:
-		#Pressed: 8
+		print("Pressed: 8")
 		routine = 8
 
 	elif code == 16603303:
-		#Pressed: 9
+		print("Pressed: 9")
 		routine = 9
 
 	elif code == 16593103:
-		#Pressed: 0
+		print("Pressed: 0")
 		routine = 0
 
-	elif code == 16605343:
-		#Pressed: Stop
+	elif code == 16599223:
+		print("Pressed: Stop")
 		routine = -1
 
 	else:
+		print("Pressed: Stop")
 		routine = -1
 	return
 
