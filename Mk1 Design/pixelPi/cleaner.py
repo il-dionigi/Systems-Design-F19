6338827fa1f6319bbee5ecedc5caaee0dd63fc80
Charles Zaloom <czaloom4@gmail.com>
2019-12-09 20:43:56 -0800

    Update cleaner.py

--- Mk1 Design/pixelPi/cleaner.py
+++ Mk1 Design/pixelPi/cleaner.py
@@ -2,9 +2,6 @@ import csv
 
 settings_address = "/home/pi/settings.csv"
 
-L = 1
-X = -1 
-Y = -1
 myID = -1 
 
 with open(settings_address, newline='') as csvfile:
@@ -14,6 +11,7 @@ with open(settings_address, newline='') as csvfile:
 
 L = 1
 f = open(settings_address, "w")
-f.write(f"L,X,Y,ID\n{L},{X},{Y},{myID}")
+f.write(f"L,X,Y,ID\n1,-1,-1,{myID}")
 f.close()
+
     
