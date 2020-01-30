197a0a2e59bc68358e6aa2e3cd4cc4af945dc2a0
1337eeab <andrewtb97@hotmail.com>
2020-01-30 15:52:56 -0800

    Update receive.py

--- rhandelin/mqtt/receive.py
+++ rhandelin/mqtt/receive.py
@@ -20,35 +20,34 @@ pixels.show()
 MQTT_SERVER = "192.168.0.100"
 MQTT_PATH = "test_channel"
 
-def Receiver():
+class Receiver():
     # The callback for when the client receives a CONNACK response from the serv$
     def __init__(self):
 
         f = open("/boot/id.txt",'r')
         self.id=int(f.readline())
-
         try:
             f = open("/boot/my_loc.txt",'r')
             my_str=f.readline()
             temp = re.findall(r'\d+', test_string) 
             my_str = list(map(int, temp)) 
-            self.my_x = my_str[0]
-            self.my_y = my_str[1]
+            self.x = my_str[0]
+            self.y = my_str[1]
             f.close()
         except:
             print("My Location is Currently Unknown, sending a REQ to ControlPy")
             self.x = None
             self.y = None
-
+        
 
         # more callbacks, etc
-        self.client = mqtt.Client(client_id = self.id)
+        self.client = mqtt.Client(client_id = str(self.id))
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         self.client.on_publish = self.on_publish
         self.client.connect(MQTT_SERVER, 1883, 60)
         
-        if self.my_x == None or self.my_y == None:
+        if self.x == None or self.y == None:
             self.client.publish("CONTROL", "get_loc")
             #Sends a publish request to get my location soon.
 
@@ -56,12 +55,11 @@ def Receiver():
         # handles reconnecting.
         # Other loop*() functions are available that give a threaded interface and a
         # manual interface.
-        self.client.loop_forever()
 
         self.commands_dict = {
         #'my_coords':
-        'rainbow':rainbow_rand,
-        'increment':pong_simulation,
+        'rainbow':self.rainbow_rand,
+        'increment':self.increment_simulation
         }
 
     def on_connect(self, client, userdata, flags, rc):
@@ -76,8 +74,8 @@ def Receiver():
 
     def rainbow_rand(self):
         for iter_id in range(200):
-            pixels.fill(((role_id + iter_id % 2)*255, (role_id + iter_id + 2 % 7)*42,
-            (role_id + 5 + iter_id % 9)*28))
+            pixels.fill(((self.id + iter_id % 2)*255, (self.id + iter_id + 2 % 7)*42,
+            (self.id + 5 + iter_id % 9)*28))
             pixels.show()
             time.sleep(0.5)
 
@@ -129,8 +127,8 @@ def Receiver():
                 temp = re.findall(r'\d+', test_string) 
                 num_list = list(map(int, temp)) 
                 if num_list[0] == id:
-                    self.my_x = num_list[1]
-                    self.my_y = num_list[2]
+                    self.x = num_list[1]
+                    self.y = num_list[2]
                     f = open("/boot/my_loc.txt",'w')
                     f.write("x: " + str(self.my_x) + " y: " + str(self.my_y))
                     f.close()
@@ -148,3 +146,8 @@ def Receiver():
                     b = num_list[3]
                     pixels.fill((r,g,b))
                     pixels.show()
+
+
+#Start Receiver
+myReceiver = Receiver()
+myReceiver.client.loop_forever()
